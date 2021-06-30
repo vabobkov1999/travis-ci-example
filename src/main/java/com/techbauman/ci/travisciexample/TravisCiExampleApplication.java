5add8d6f0947476422d194a4e22bc716c40411de
vasily1223 <vasilybobkov1@gmail.com>
2021-06-30 15:13:45 +0300

    [add] travis ci support

---
+++ src/main/java/com/techbauman/ci/travisciexample/TravisCiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techbauman.ci.travisciexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techbauman/ci/travisciexample/TravisCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbauman.ci.travisciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
